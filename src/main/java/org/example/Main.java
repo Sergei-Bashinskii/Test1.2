28f59dc6a17034d6df40899f1edc57ccd02bdf61
Sergei-Bashinskii <sergei13121390@gmail.com>
2024-08-21 04:49:39 +0300

    Task3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,20 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int[] arres = {1, 2, 3, 4, 5};
-
         int levo = 0;
         int pravo = arres.length - 1;
-
         while (levo < pravo) {
-
             int zero = arres[levo];
             arres[levo] = arres[pravo];
             arres[pravo] = zero;
-
             levo++;
             pravo--;
         }
-
         for (int i : arres) {
             System.out.println(i);
         }
